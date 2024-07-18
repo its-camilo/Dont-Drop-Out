212c8cc82f8a9dc2d91205d6845393c21697b06c
J101405 <jrd101405@gmail.com>
2024-07-17 22:40:26 -0500

    Mecanica de clonación y detalles menores

--- Assets/Code/Scripts/PlayerController.cs
+++ Assets/Code/Scripts/PlayerController.cs
@@ -2,50 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerController : MonoBehaviour
 {
-    float speedMovement = 50f;
-    float speedRotation = 140f;
-    float gravity = 50f;
-    float jumpForce = 15f;
+    public GameObject clone;
+    public GameObject spawn;
+    bool grounded;
+    float speedMovement = 30f;
+    float speedRotation = 110f;
+    float gravity = 60f;
+    float jumpForce = 20f;
     Vector3 jumpVector = Vector3.zero;
     CharacterController cc;
 
     private void Awake()
     {
         cc = GetComponent<CharacterController>();
+        spawn = GameObject.Find("Spawn");
+    }
+
+    private void Start()
+    {
+        transform.SetPositionAndRotation(spawn.transform.position, new Quaternion(0,1,0,1));
     }
 
     // Update is called once per frame
     void Update()
     {
+        LayerMask mask = LayerMask.GetMask("Jumpable");
+
+        if (Physics.Raycast(transform.position, Vector3.down, 0.9f, mask))
+        {
+            grounded = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) 
+        {
+            Respawn(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Respawn(false);
+        }
+
         if (Input.GetKey(KeyCode.W))
         {
-            cc.Move((transform.forward * speedMovement * Time.deltaTime));
+            cc.Move(speedMovement * Time.deltaTime * transform.forward);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-           cc.Move(transform.forward * -speedMovement * Time.deltaTime);
+            cc.Move(-speedMovement * Time.deltaTime * transform.forward);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.up * speedRotation * Time.deltaTime);
+            transform.Rotate(speedRotation * Time.deltaTime * Vector3.up);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.down * speedRotation * Time.deltaTime);
+            transform.Rotate(speedRotation * Time.deltaTime * Vector3.down);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
         {
-            jumpVector.y = jumpForce; 
+            grounded = false;
+            jumpVector.y = jumpForce;
         }
 
         jumpVector.y -= gravity * Time.deltaTime;
-        cc.Move(jumpVector*Time.deltaTime);
+        cc.Move(jumpVector * Time.deltaTime);
     }
-    
+
+    void Respawn(bool cloner)
+    {
+        if (cloner == false)
+        {
+            cc.enabled = false;
+            transform.SetPositionAndRotation(spawn.transform.position, new Quaternion(0, 1, 0, 1));
+            cc.enabled = true;
+        }
+        if (cloner == true)
+        {
+            cc.enabled = false;
+            Instantiate(clone,transform.position,Quaternion.identity);
+            transform.SetPositionAndRotation(spawn.transform.position, new Quaternion(0, 1, 0, 1));
+            cc.enabled = true;
+        }
+
+    }
 }
