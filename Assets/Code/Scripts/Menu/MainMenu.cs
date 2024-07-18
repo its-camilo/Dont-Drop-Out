313836af86c888d5b192e0e1420fb88d9e631d67
Camilo Alejandro <camiloalejandrolagosmalaver@gmail.com>
2024-07-17 20:42:23 -0500

    xd

--- Assets/Code/Scripts/Menu/MainMenu.cs
+++ Assets/Code/Scripts/Menu/MainMenu.cs
@@ -5,13 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene("LoadingScreen");
     }
 
-    // Update is called once per frame
     public void Quit()
     {
         Application.Quit();
