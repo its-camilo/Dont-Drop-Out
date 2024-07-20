04657434f1e25c48ffabfd3d5fb16b475a4f1789
J101405 <jrd101405@gmail.com>
2024-07-19 22:15:26 -0500

    Dialogue correcto

--- Assets/Code/Scripts/Dialogue.cs
+++ Assets/Code/Scripts/Dialogue.cs
@@ -11,11 +11,7 @@ public class Dialogue : MonoBehaviour
     [SerializeField] private AudioSource dialogueBlop, dialogueHuesos;
     [SerializeField] private GameObject blop, huesos;
 
-<<<<<<< Updated upstream
-    private float typingTime = 0.00000000000001f;
-=======
     private float typingTime = 0.2f;
->>>>>>> Stashed changes
     private int lineIndex;
     void Start()
     {
@@ -26,111 +22,32 @@ public class Dialogue : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == "Level1")
         {
-<<<<<<< Updated upstream
-            if (lineIndex == 1 || lineIndex == 3 || lineIndex == 4 || lineIndex == 5 || lineIndex == 7 || lineIndex == 9 || lineIndex == 11 || lineIndex == 13 || lineIndex == 14 || lineIndex == 17 || lineIndex == 21 || lineIndex == 23) //huesos
-            {
-                dialogueBlop.Stop();
-                blop.SetActive(false);
-                if (!dialogueHuesos.isPlaying)
-                {
-                    dialogueHuesos.Play();
-                    huesos.SetActive(true);
-                }
-            }
-
-            else if (lineIndex == 0 || lineIndex == 2 || lineIndex == 6 || lineIndex == 10 || lineIndex == 15 || lineIndex == 20) //blop
-            {
-                huesos.SetActive(false);
-                dialogueHuesos.Stop();
-                if (!dialogueBlop.isPlaying)
-                {
-                    blop.SetActive(true);
-                    dialogueBlop.Play();
-                }
-            }
-
-            else
-            {
-                huesos.SetActive(false);
-                blop.SetActive(false);
-                dialogueHuesos.Stop();
-                dialogueBlop.Stop();
-            }
-
-            if (Input.GetKeyDown(KeyCode.F) && lineIndex != 8 && lineIndex != 12 && lineIndex != 16 && lineIndex != 19 && lineIndex != 20 && lineIndex != 22)
-=======
-            if (Input.GetKeyDown(KeyCode.F))
->>>>>>> Stashed changes
             {
                 if (dialogueText.text == dialogueLines[lineIndex])
                 {
                     NextDialogueLine();
                 }
 
-                //else
-                //{
-                //    StopAllCoroutines();
-                //    dialogueText.text = dialogueLines[lineIndex];
-                //}
+                else
+                {
+                    StopAllCoroutines();
+                    dialogueText.text = dialogueLines[lineIndex];
+                }
             }
         }
 
         if (SceneManager.GetActiveScene().name == "Level2")
         {
-<<<<<<< Updated upstream
-            if (lineIndex == 0 || lineIndex == 1 || lineIndex == 2 || lineIndex == 5 || lineIndex == 7 || lineIndex == 9 || lineIndex == 10) //huesos
-            {
-                blop.SetActive(false);
-                dialogueBlop.Stop();
-                if (!dialogueHuesos.isPlaying)
-                {
-                    huesos.SetActive(true);
-                    dialogueHuesos.Play();
-                }
-            }
-
-            else if (lineIndex == 3 || lineIndex == 4) //blop
-            {
-                huesos.SetActive(false);
-                dialogueHuesos.Stop();
-                if (!dialogueBlop.isPlaying)
-                {
-                    blop.SetActive(true);
-                    dialogueBlop.Play();
-                }
-            }
-
-            else
-            {
-                huesos.SetActive(false);
-                blop.SetActive(false);
-                dialogueHuesos.Stop();
-                dialogueBlop.Stop();
-            }
-
-            if (Input.GetKeyDown(KeyCode.F) && lineIndex != 3 && lineIndex != 6 && lineIndex != 8 && lineIndex != 9)
-=======
-            if (Input.GetKeyDown(KeyCode.F))
->>>>>>> Stashed changes
             {
                 if (dialogueText.text == dialogueLines[lineIndex])
                 {
                     NextDialogueLine();
                 }
-<<<<<<< Updated upstream
-
-                //else
-                //{
-                //    StopAllCoroutines();
-                //    dialogueText.text = dialogueLines[lineIndex];
-                //}
-=======
                 else
                 {
                     StopAllCoroutines();
                     dialogueText.text = dialogueLines[lineIndex];
                 }
->>>>>>> Stashed changes
             }
         }
 
@@ -147,53 +64,6 @@ public class Dialogue : MonoBehaviour
                     StopAllCoroutines();
                     dialogueText.text = dialogueLines[lineIndex];
                 }
-            }
-        }
-
-        if (SceneManager.GetActiveScene().name == "Level3")
-        {
-            if (lineIndex == 1 ||lineIndex == 2 || lineIndex == 3 || lineIndex == 5 || lineIndex == 6 || lineIndex == 8 || lineIndex == 9 || lineIndex == 10 || lineIndex == 11) //huesos
-            {
-                blop.SetActive(false);
-                dialogueBlop.Stop();
-                if (!dialogueHuesos.isPlaying)
-                {
-                    huesos.SetActive(true);
-                    dialogueHuesos.Play();
-                }
-            }
-
-            else if (lineIndex == 0) //blop
-            {
-                huesos.SetActive(false);
-                dialogueHuesos.Stop();
-                if (!dialogueBlop.isPlaying)
-                {
-                    blop.SetActive(true);
-                    dialogueBlop.Play();
-                }
-            }
-
-            else
-            {
-                huesos.SetActive(false);
-                blop.SetActive(false);
-                dialogueHuesos.Stop();
-                dialogueBlop.Stop();
-            }
-
-            if (Input.GetKeyDown(KeyCode.F) && lineIndex != 2 && lineIndex != 4 && lineIndex != 7 && lineIndex != 8 && lineIndex != 10)
-            {
-                if (dialogueText.text == dialogueLines[lineIndex])
-                {
-                    NextDialogueLine();
-                }
-
-                //else
-                //{
-                //    StopAllCoroutines();
-                //    dialogueText.text = dialogueLines[lineIndex];
-                //}
             }
         }
     }
